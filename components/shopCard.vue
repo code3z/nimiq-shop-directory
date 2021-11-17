548c9dba7d88fd7aaf67997bf6c426cb3a1e6421
code3z <code3@vivaldi.net>
2021-11-17 17:09:58 -0500

    feat: search, multiple locations

---
+++ components/shopCard.vue
@@ -0,0 +1,14 @@
+ <template>
+  <v-card></v-card>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    shop: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,9 @@
         <v-card-actions>
           <v-btn :href="shop.website" target="_blank">Shop Now</v-btn>
           <v-btn
-            :href="'https://arcanemaps.com#place--' + shop.location.latitude + ',' + shop.location.longitude"
+            v-for="shopInstance in toArray(shop.location)"
+            :key="shopInstance.latitude"
+            :href="'https://arcanemaps.com#place--' + shopInstance.latitude + ',' + shopInstance.longitude"
             target="_blank"
           >View on Map</v-btn>
         </v-card-actions>
@@ -31,18 +33,53 @@
 
 <script setup lang="ts">
 import supabase from '@/lib/supabase'
-import { ref } from '@nuxt/bridge/dist/runtime'
+import { ref, useRoute } from '@nuxt/bridge/dist/runtime'
+import { PostgrestResponse } from '@supabase/supabase-js'
+
 /*async function asyncData() {
   let { data, error, status } = await supabase.from("shops").select()
   console.log(data)
   return reactive(data)
 }*/
+interface location {
+  latitude: string,
+  longitude: string
+}
+interface shop {
+  id: string, // uuid v4
+  name: string,
+  type: number, // 1: online-only, 2: retail, 3: both
+  phone: number,
+  email: string,
+  website: string,
+  description: string,
+  shippingCountries: string[],
+  location: location | location[],
+  // fts: full text search potgres
+}
 
 let data = ref<any[] | null>()
-supabase.from("shops").select().then((response) => {
+const callback = (response: PostgrestResponse<any>) => {
   console.log(response.body)
   data.value = response.body
-})
+}
+const route = useRoute()
+
+function toArray(data: any) {
+  if (Array.isArray(data)) {
+    return data
+  } else {
+    return [data]
+  }
+}
+if (route.query.search) {
+  supabase.from("shops").select().textSearch('fts', route.query.search as string, {
+    type: 'websearch',
+    config: 'english'
+  }).then(callback)
+} else {
+  supabase.from("shops").select().then(callback)
+}
 </script>
 
 <style>
