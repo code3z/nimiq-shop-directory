9c9a5abb032a87597a13695dc9573c4af69ffbbd
code3z <code3@vivaldi.net>
2021-11-18 11:02:38 -0500

    feat: login

---
+++ pages/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-card>
+      <h1>Login</h1>
+      <p>Please enter your buisness email. This will be the email you log in with AND the contact email displayed on your shop's profile.</p>
+      <v-text-field type="email" label="Email" v-model="email"></v-text-field>
+      <button class="nq-button" @click="signIn">Log In</button>
+    </v-card>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+import supabase from '@/lib/supabase'
+import { ref } from '@nuxt/bridge/dist/runtime'
+
+const email = ref()
+async function signIn() {
+  await supabase.auth.signIn({
+    email: email.value,
+  })
+}
+</script>
