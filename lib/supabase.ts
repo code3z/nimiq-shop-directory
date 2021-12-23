aa151e70cc26d6fe94444994b5e91eb2509a0c7d
code3z <code3@vivaldi.net>
2021-12-23 15:43:29 -0500

    try a new supabase instance

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 
-const supabaseUrl = 'https://vvwxrsgpuljkefpxuyrr.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAxNzU2NSwiZXhwIjoxOTUyNTkzNTY1fQ.pbRmf3zv2tJ4HcT0lZ2Nd6S48CClj40EL8UmYinYo0o'
+const supabaseUrl = 'https://fuzexexbjrlajnfabbtj.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDEzMTc3NiwiZXhwIjoxOTU1NzA3Nzc2fQ.U0x5kwhdS6ZUqdK-1pAaCSoSWQlmOJHAJd55ZxUbAr4'
 
 
 export default createClient(supabaseUrl, supabaseAnonKey)
